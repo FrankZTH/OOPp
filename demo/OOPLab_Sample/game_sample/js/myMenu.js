8cb884061d93eef37d18eb466d03973e7c811084
FRANK-PC\Frank <frankboygx@gmail.com>
2017-04-14 00:25:12 +0800

    no message

--- demo/OOPLab_Sample/game_sample/js/myMenu.js
+++ demo/OOPLab_Sample/game_sample/js/myMenu.js
@@ -19,19 +19,8 @@ var MyMenu = Framework.exClass(Framework.GameMainMenu , {
 
 	load: function(){
 		//Animation Sprite會用到的圖片資源
-        var photoLink =
-        [
-            define.imagePath + 'NTUT1.png'
-        ];
 
-        this.scrollBar = new Framework.Sprite(define.imagePath + 'scrollBar.png');
-        this.rightArrow = new Framework.Sprite(define.imagePath + 'rightArrow.png');
-        this.photo = new Framework.AnimationSprite({url: photoLink});
-
-		this.isTouchArrow = false;
-        this.previousTouch = { x: 0, y: 0 };
-        this.currentTouch = { x: 0, y: 0 };
-
+				this.startGame = new Framework.Sprite(define.imagePath + 'NTUT1.png');
         //為了讓之後的位置較好操控, new出一個位於中心點且可以黏貼任何東西的容器
         //注意, Position都是用中心點
         this.center = new Framework.Scene();  //背景圖中心
@@ -39,19 +28,13 @@ var MyMenu = Framework.exClass(Framework.GameMainMenu , {
             x: Framework.Game.getCanvasWidth() / 2,
             y: Framework.Game.getCanvasHeight() / 2
         };
-
+				this.startGame.scale=0.7;
         //由於scrollBar將會被attach到this.center上
         //故x設為0, 表示x也是要正中心
 
-        this.scrollBar.position = {
-            x: Framework.Game.getCanvasWidth() / 2 - 10,
-            y: Framework.Game.getCanvasHeight() / 4 * 3.5
-        };
-
-        this.photo.scale = 0.7;
-        this.photo.position = {
-            x: 0,
-            y: 0
+        this.startGame.position = {
+            x: this.center.x,
+            y: this.center.y
         };
 
         this.audio = new Framework.Audio({
@@ -61,18 +44,10 @@ var MyMenu = Framework.exClass(Framework.GameMainMenu , {
         });
 
         this.audio.play({name: 'song1', loop: true});
-
-        this.rightArrow.scale = 0.15;
-        this.rightArrow.position = {
-            x: Framework.Game.getCanvasWidth() / 2 - 400,
-            y: Framework.Game.getCanvasHeight() / 4 * 3.5
-        };
-        this.center.attach(this.photo);
+        this.center.attach(this.startGame);
         this.rootScene.attach(this.center);
-        this.rootScene.attach(this.scrollBar);
-        this.rootScene.attach(this.rightArrow);
 
-        this.photo.start();
+        //this.photo.start();
 
 	},
 
@@ -81,42 +56,20 @@ var MyMenu = Framework.exClass(Framework.GameMainMenu , {
     },
 
     update:function(){
-        this.rootScene.update();
-        this.scrollBar.update();
     },
 
     draw: function(parentCtx) {
         this.rootScene.draw(parentCtx);
     },
 
-    mousedown: function(e) {
+    click: function(e) {
         if (e) {
             console.log(e.x, e.y);
         }
         this.previousTouch = { x: e.x, y: e.y };
-        if (this.previousTouch.x > this.rightArrow.upperLeft.x && this.previousTouch.x < this.rightArrow.upperRight.x && this.previousTouch.y > this.rightArrow.upperLeft.y && this.previousTouch.y < this.rightArrow.lowerLeft.y) {
-            this.isTouchArrow = true;
+        if (e.x>=this.startGame.position.x) {
+            Framework.Game.goToNextLevel();
         }
-    },
-
-    mousemove: function(e) {
-        if (this.isTouchArrow) {
-            this.currentTouch = { x: e.x, y: e.y };
-            if (this.currentTouch.x > this.previousTouch.x && this.currentTouch.y < this.rightArrow.lowerLeft.y && this.currentTouch.y > this.rightArrow.upperLeft.y) {
-                //當arrow被Touch到時, 會跟隨著觸控的位置移動
-                this.rightArrow.position.x = this.rightArrow.position.x + this.currentTouch.x - this.previousTouch.x
-                if(this.currentTouch.x > Framework.Game.getCanvasWidth() - this.rightArrow.width - 150) { //改變拉條長度
-                    //當要換關時, 可以呼叫goToNextLevel, goToPreviousLevel, goToLevel(levelName)
-                    this.audio.pauseAll();
-                    Framework.Game.goToNextLevel();
-                }
-            }
-       }
-       this.previousTouch = this.currentTouch;
-    },
-
-    mouseup: function(e) {
-        this.isTouchArrow = false;
     },
 
     touchstart: function (e) {
